f0cc7cc200680dca4f638c5ce97d5e69c7e3f7c8
Akndju <davidd.akindoju@gmail.com>
2020-08-30 01:00:58 +0100

    added functionality

--- script.js
+++ script.js
@@ -43,4 +43,73 @@ function searchMeals(e) {
   }
 }
 
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
+function addMealToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  singleMeal.innerHTML = `
+    <div class ="single-meal">
+        <h1>${meal.strMeal}</h1>
+        <img src ="${meal.strMealThumb}" alt = "${meal.strMeal}"/>
+        <div class = "single-meal-info">
+            ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+            ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+        </div>
+        <div>
+            <p>${meal.strInstructions}</p>
+             <h2>Ingredients</h2>
+             <ul>
+                ${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
+             </ul>
+        </div>
+    </div>`;
+}
+
+function getRandomMeal() {
+  meals.innerHTML = '';
+  resultHeading.innerHTML = '';
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
 submit.addEventListener('submit', searchMeals);
+random.addEventListener('click', getRandomMeal);
+
+meals.addEventListener('click', (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
+    }
+  });
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealid');
+    getMealById(mealID);
+  }
+});
